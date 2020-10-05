2b7943f4f1a31db2da1f6c22a206340ae43742a4
Pascal Joret <pjoret@sqli.com>
2020-10-05 17:26:54 +0200

    Expanded - onPressed -setState

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,51 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue[800],
+        title: Center(
+          child: Text('Ask me anything'),
+        ),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print('I got clicked');
+              },
+              child: Center(
+                child: Image.asset('images/ball1.png'),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
